f312e418b49b5c7931b3ea547f1823d61e67e9d5
Yuji Nakayama <nkymyj@gmail.com>
2015-10-08 18:28:07 +0900

    Introduce Mocha and Chai

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,21 @@
 import gulp from 'gulp'
 
+let sourcePattern = 'src/*.js'
+let testPattern = 'test/*.js'
+
 gulp.task('compile', () => {
   let babel = require('gulp-babel')
-  return gulp.src('src/*.js')
+  return gulp.src(sourcePattern)
     .pipe(babel())
     .pipe(gulp.dest('lib'))
+})
+
+gulp.task('test', () => {
+  let mocha = require('gulp-mocha')
+  return gulp.src(testPattern)
+    .pipe(mocha())
+})
+
+gulp.task('watch', () => {
+  gulp.watch([sourcePattern, testPattern], ['test'])
 })
